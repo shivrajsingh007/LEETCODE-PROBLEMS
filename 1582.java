4676e76b035e6b4f284ffc91d8c85c425aac92f7
shivrajsingh007 <shivrajsingh1458@gmail.com>
2026-03-04 20:49:39 +0530

    Special Positions in a Binary Matrix

---
+++ 1582.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int numSpecial(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        int[] rowCount = new int[m];
+        int[] colCount = new int[n];
+
+        // Count number of 1s in each row and column
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1) {
+                    rowCount[i]++;
+                    colCount[j]++;
+                }
+            }
+        }
+
+        int result = 0;
+
+        // Check special positions
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1 && rowCount[i] == 1 && colCount[j] == 1) {
+                    result++;
+                }
+            }
+        }
+
+        return result;
+    }
+}
