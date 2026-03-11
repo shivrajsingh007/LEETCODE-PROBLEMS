cc10119981eba6d9d9edda695d6aa10b09d58b2e
shivrajsingh007 <shivrajsingh1458@gmail.com>
2026-03-11 23:00:36 +0530

    Majority Element

---
+++ 169.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+class Solution {
+    public int majorityElement(int nums[]){
+        Arrays.sort(nums);
+        int mid = nums.length/2;
+        return nums[mid];
+    }
+}
